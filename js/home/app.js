b30e44e9d89662e4f504e5fbc344745044188877
SaiZack <100429130+SaiZack@users.noreply.github.com>
2023-09-24 16:20:42 +0000

    add menu

---
+++ js/home/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#pokemon-caro').owlCarousel({
+        loop: true,
+        margin: 100,
+        autoWidth: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText: ["<img src='../img/triangle.png' width='50'>", "<img src='../img/triangle.png' width='50'>"]
+    });
+})
--- js/methods.js
+++ js/methods.js
@@ -1,10 +1,13 @@
-function addForm(modal, place) {
-    $(modal).find('.modal-title').text("Add Pokemon")
-    $(modal).find('form').removeClass('d-none')
-    $(modal).find('.add-btn').removeClass('d-none')
-    $(modal).find('.add-btn').data('place', place)
-}
-function removeForm(modal) {
-    $(modal).find('form').addClass('d-none')
-    $(modal).find('.add-btn').addClass('d-none')
-}
+// function addForm(modal, place) {
+//     $(modal).find('.modal-title').text("Add Pokemon")
+//     $(modal).find('form').removeClass('d-none')
+//     $(modal).find('.add-btn').removeClass('d-none')
+//     $(modal).find('.add-btn').data('place', place)
+// }
+// function removeForm(modal) {
+//     $(modal).find('form').addClass('d-none')
+//     $(modal).find('.add-btn').addClass('d-none')
+// }
+// function addPokemon(place){
+    
+// }
